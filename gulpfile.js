e1516b22bbc0354e73e65d306173ae047765be9c
dofun <306175578@qq.com>
2018-09-07 00:32:23 +0800

    fix watch for html

--- gulpfile.js
+++ gulpfile.js
@@ -22,87 +22,87 @@ var distPath = "dist/", // 输出目录
   assetPath = "asset/"; // 静态资源
 
 // css 压缩
-gulp.task("minify:css", function() {
+gulp.task("minify:css", function () {
   return (
     gulp
-      .src("css/**/*") // css 文件地址
-      .pipe(changed(distPath + "css/**/*"))
-      //.pipe(concat("build.css")) //合成到一个 css，合并顺序按读取到文件的先后排列
-      //.pipe(gulp.dest(distPath + "css/"))
-      .pipe(
-        postcss([
-          cssnext({
-            browsers: ["> 3%", "last 5 versions", "Firefox ESR"],
-            warnForDuplicates: false
-          }),
-          cssnano()
-        ])
-      )
-      //.pipe(concat("build.min.css")) //压缩后的 css
-      .pipe(gulp.dest(distPath + "css/"))
+    .src("css/**/*") // css 文件地址
+    .pipe(changed(distPath + "css/**/*"))
+    //.pipe(concat("build.css")) //合成到一个 css，合并顺序按读取到文件的先后排列
+    //.pipe(gulp.dest(distPath + "css/"))
+    .pipe(
+      postcss([
+        cssnext({
+          browsers: ["> 3%", "last 5 versions", "Firefox ESR"],
+          warnForDuplicates: false
+        }),
+        cssnano()
+      ])
+    )
+    //.pipe(concat("build.min.css")) //压缩后的 css
+    .pipe(gulp.dest(distPath + "css/"))
   );
 });
 
 // css 压缩并添加 md5
-gulp.task("minify:css:md5", function() {
+gulp.task("minify:css:md5", function () {
   return (
     gulp
-      .src("css/**/*") // css 文件地址
-      .pipe(changed(distPath + "css/**/*"))
-      //.pipe(concat("build.css")) //合成到一个 css，合并顺序按读取到文件的先后排列
-      //.pipe(gulp.dest(distPath + "css/"))
-      .pipe(
-        postcss([
-          cssnext({
-            browsers: ["> 1%", "last 6 versions", "Firefox ESR"],
-            warnForDuplicates: false
-          }),
-          cssnano()
-        ])
-      )
-      .pipe(rev()) //文件名加MD5后缀
-      //.pipe(concat("build.min.css")) //压缩后的 css
-      .pipe(gulp.dest(distPath + "css/"))
-      .pipe(rev.manifest("rev-css-manifest.json")) //生成一个rev-manifest.json
-      .pipe(gulp.dest("rev")) //将 rev-manifest.json 保存到 rev 目录内
+    .src("css/**/*") // css 文件地址
+    .pipe(changed(distPath + "css/**/*"))
+    //.pipe(concat("build.css")) //合成到一个 css，合并顺序按读取到文件的先后排列
+    //.pipe(gulp.dest(distPath + "css/"))
+    .pipe(
+      postcss([
+        cssnext({
+          browsers: ["> 1%", "last 6 versions", "Firefox ESR"],
+          warnForDuplicates: false
+        }),
+        cssnano()
+      ])
+    )
+    .pipe(rev()) //文件名加MD5后缀
+    //.pipe(concat("build.min.css")) //压缩后的 css
+    .pipe(gulp.dest(distPath + "css/"))
+    .pipe(rev.manifest("rev-css-manifest.json")) //生成一个rev-manifest.json
+    .pipe(gulp.dest("rev")) //将 rev-manifest.json 保存到 rev 目录内
   );
 });
 
 // js压缩
-gulp.task("minify:js", function() {
+gulp.task("minify:js", function () {
   return (
     gulp
-      .src("js/**/*") // js文件地址
-      .pipe(changed(distPath + "js/**/*"))
-      //.pipe(concat("build.js")) //合成到一个 js，合并顺序按读取到文件的先后排列
-      //.pipe(gulp.dest(distPath + "js/"))
-      .pipe(babel())
-      .pipe(uglify())
-      //.pipe(concat("build.min.js")) //压缩后的js
-      .pipe(gulp.dest(distPath + "js/"))
+    .src("js/**/*") // js文件地址
+    .pipe(changed(distPath + "js/**/*"))
+    //.pipe(concat("build.js")) //合成到一个 js，合并顺序按读取到文件的先后排列
+    //.pipe(gulp.dest(distPath + "js/"))
+    .pipe(babel())
+    .pipe(uglify())
+    //.pipe(concat("build.min.js")) //压缩后的js
+    .pipe(gulp.dest(distPath + "js/"))
   );
 });
 
 // js 压缩并添加 md5
-gulp.task("minify:js:md5", function() {
+gulp.task("minify:js:md5", function () {
   return (
     gulp
-      .src("js/**/*") // js文件地址
-      .pipe(changed(distPath + "js/**/*"))
-      //.pipe(concat("build.js")) //合成到一个js，合并顺序按读取到文件的先后排列
-      //.pipe(gulp.dest(distPath + "js/"))
-      .pipe(babel())
-      .pipe(uglify())
-      .pipe(rev()) //文件名加MD5后缀
-      //.pipe(concat("build.min.js")) //压缩后的js
-      .pipe(gulp.dest(distPath + "js/"))
-      .pipe(rev.manifest("rev-js-manifest.json")) //生成一个rev-manifest.json
-      .pipe(gulp.dest("rev")) //将 rev-manifest.json 保存到 rev 目录内
+    .src("js/**/*") // js文件地址
+    .pipe(changed(distPath + "js/**/*"))
+    //.pipe(concat("build.js")) //合成到一个js，合并顺序按读取到文件的先后排列
+    //.pipe(gulp.dest(distPath + "js/"))
+    .pipe(babel())
+    .pipe(uglify())
+    .pipe(rev()) //文件名加MD5后缀
+    //.pipe(concat("build.min.js")) //压缩后的js
+    .pipe(gulp.dest(distPath + "js/"))
+    .pipe(rev.manifest("rev-js-manifest.json")) //生成一个rev-manifest.json
+    .pipe(gulp.dest("rev")) //将 rev-manifest.json 保存到 rev 目录内
   );
 });
 
 // image压缩
-gulp.task("minify:img", function() {
+gulp.task("minify:img", function () {
   return gulp
     .src([
       "image/**/*.jpg",
@@ -114,18 +114,30 @@ gulp.task("minify:img", function() {
       cache(
         imagemin(
           [
-            imagemin.gifsicle({ interlaced: true }), //类型：Boolean 默认：false 隔行扫描gif进行渲染
-            imagemin.jpegtran({ progressive: true }), //类型：Boolean 默认：false 无损压缩jpg图片
-            imagemin.optipng({ optimizationLevel: 3 }), //类型：Number  默认：3  取值范围：0-7（优化等级）
+            imagemin.gifsicle({
+              interlaced: true
+            }), //类型：Boolean 默认：false 隔行扫描gif进行渲染
+            imagemin.jpegtran({
+              progressive: true
+            }), //类型：Boolean 默认：false 无损压缩jpg图片
+            imagemin.optipng({
+              optimizationLevel: 3
+            }), //类型：Number  默认：3  取值范围：0-7（优化等级）
             imagemin.svgo({
-              plugins: [
-                { removeViewBox: true }, // removeViewBox 移除svg的viewbox属性
-                { cleanupIDs: false }, // 删除未使用的和缩小使用的ID
-                { multipass: true } //类型：Boolean 默认：false 多次优化svg直到完全优化
+              plugins: [{
+                  removeViewBox: true
+                }, // removeViewBox 移除svg的viewbox属性
+                {
+                  cleanupIDs: false
+                }, // 删除未使用的和缩小使用的ID
+                {
+                  multipass: true
+                } //类型：Boolean 默认：false 多次优化svg直到完全优化
               ]
             })
-          ],
-          { use: [pngquant()] }
+          ], {
+            use: [pngquant()]
+          }
         )
       )
     )
@@ -133,7 +145,7 @@ gulp.task("minify:img", function() {
 });
 
 // image压缩并添加 md5
-gulp.task("minify:img:md5", function() {
+gulp.task("minify:img:md5", function () {
   return gulp
     .src([
       "image/**/*.jpg",
@@ -145,18 +157,30 @@ gulp.task("minify:img:md5", function() {
       cache(
         imagemin(
           [
-            imagemin.gifsicle({ interlaced: true }), //类型：Boolean 默认：false 隔行扫描gif进行渲染
-            imagemin.jpegtran({ progressive: true }), //类型：Boolean 默认：false 无损压缩jpg图片
-            imagemin.optipng({ optimizationLevel: 5 }), //类型：Number  默认：3  取值范围：0-7（优化等级）
+            imagemin.gifsicle({
+              interlaced: true
+            }), //类型：Boolean 默认：false 隔行扫描gif进行渲染
+            imagemin.jpegtran({
+              progressive: true
+            }), //类型：Boolean 默认：false 无损压缩jpg图片
+            imagemin.optipng({
+              optimizationLevel: 5
+            }), //类型：Number  默认：3  取值范围：0-7（优化等级）
             imagemin.svgo({
-              plugins: [
-                { removeViewBox: true }, // removeViewBox 移除svg的viewbox属性
-                { cleanupIDs: false }, // 删除未使用的和缩小使用的ID
-                { multipass: true } //类型：Boolean 默认：false 多次优化svg直到完全优化
+              plugins: [{
+                  removeViewBox: true
+                }, // removeViewBox 移除svg的viewbox属性
+                {
+                  cleanupIDs: false
+                }, // 删除未使用的和缩小使用的ID
+                {
+                  multipass: true
+                } //类型：Boolean 默认：false 多次优化svg直到完全优化
               ]
             })
-          ],
-          { use: [pngquant()] }
+          ], {
+            use: [pngquant()]
+          }
         )
       )
     )
@@ -167,7 +191,7 @@ gulp.task("minify:img:md5", function() {
 });
 
 // 执行html压缩
-gulp.task("minify:html", function() {
+gulp.task("minify:html", function () {
   return gulp
     .src("*.html")
     .pipe(
@@ -202,39 +226,50 @@ gulp.task("minify:html", function() {
 // });
 
 //使用rev替换 md5文件名，包括 html 和 css 的资源文件
-gulp.task("rev:html", function() {
+gulp.task("rev:html", function () {
   //html，针对js,css,img
   return gulp
     .src(["rev/**/*.json", distPath + "**/*.html"])
-    .pipe(revCollector({ replaceReved: true }))
+    .pipe(revCollector({
+      replaceReved: true
+    }))
     .pipe(gulp.dest(distPath));
 });
 
-gulp.task("rev:css", function() {
+gulp.task("rev:css", function () {
   //css，主要是针对img替换
   return gulp
     .src(["rev/**/rev-img-manifest.json", distPath + "css/*.css"])
-    .pipe(revCollector({ replaceReved: true }))
+    .pipe(revCollector({
+      replaceReved: true
+    }))
     .pipe(gulp.dest(distPath + "css"));
 });
 
 // 删除 dist 文件
-gulp.task("clean:dist", function(cb) {
-  return gulp.src(distPath, { read: false }).pipe(clean());
+gulp.task("clean:dist", function (cb) {
+  return gulp.src([distPath,"rev"], {
+    read: false
+  }).pipe(clean());
+});
+
+// 清除缓存
+gulp.task('clear:cache',function(){
+  cache.clearAll()
 });
 
 //复制静态文件
-gulp.task("copy:asset", function() {
+gulp.task("copy:asset", function () {
   return gulp.src(assetPath + "**/*").pipe(gulp.dest(distPath + "asset/"));
 });
 
 //复制字体文件
-gulp.task("copy:fonts", function() {
+gulp.task("copy:fonts", function () {
   return gulp.src(assetPath + "**/*").pipe(gulp.dest(distPath + "fonts"));
 });
 
 //复制图片文件
-gulp.task("copy:img", function() {
+gulp.task("copy:img", function () {
   //如果需要压缩图片，那么这步可以省略
   return gulp.src("image/**/*").pipe(gulp.dest(distPath + "image"));
 });
@@ -244,8 +279,8 @@ gulp.task("copy:img", function() {
 // });
 
 //监视文件的变化，有修改时，自动调用default缺省默认任务
-gulp.task("watch", function() {
-  gulp.watch("**/*.html", ["default"]);
+gulp.task("watch", function () {
+  gulp.watch(["asset/**/*","css/**/*","fonts/**/*","image/**/*","js/**/*","*.html"], ["default"]);
 });
 
 //缺省默认任务，输出的js和css文件都带参数
@@ -261,7 +296,7 @@ gulp.task("watch", function() {
  * 9、使用gulp-rev-collectorc插件对刚才生成带参数的js和css文件在html页面上进行替换，如build.min.js替换成build-asdf123.min.js。输出到 dist 目录。
  * 10、（可选）使用gulp-rev-collectorc插件对刚才生成带参数的img文件在css文件上进行替换，如common.jpg替换成common-asdf12.jpg。输出到目录
  * */
-gulp.task("default", function(cb) {
+gulp.task("default", function (cb) {
   seq(
     "clean:dist",
     "copy:asset",
@@ -271,6 +306,7 @@ gulp.task("default", function(cb) {
     "minify:img:md5",
     "minify:html",
     "rev:html",
-    "rev:css"
+    "rev:css",
+    "watch"
   )(cb);
 });
